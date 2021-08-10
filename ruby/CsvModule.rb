dc2b5d43d14c82ad6d028194142e55dbbbb4077d
wutaotao <531618500@qq.com>
2021-08-10 22:41:10 +0800

    csv read file with module, module can add class methods and instance methods with extend, include keywords.

--- ruby/CsvModule.rb
+++ ruby/CsvModule.rb
@@ -1,12 +1,42 @@
 module AcsAsCsv
 
   def self.included(base)
-    base extend ClassMethod
+    # need to use extend for later call in Csv class
+    base.extend ClassMethod
   end
 
   module ClassMethod
+    # the above extend keyword make the csv_base here become class method
     def csv_base
+      # same use instance method with include
+      include InstanceMethod
+    end
+  end
 
+  module InstanceMethod
+    def read
+      file = File.new(self.class.to_s.downcase + '.txt')
+      @head = file.gets.chomp.split(', ')
+      @content = []
+      file.each do |l|
+        @content << l.chomp.split(', ')
+      end
+    end
+
+    attr_accessor :head, :content
+
+    def initialize
+      read
     end
   end
 end
+
+class Csv
+  # very flexible
+  include AcsAsCsv
+  csv_base
+end
+
+c = Csv.new
+p c.head
+p c.content
