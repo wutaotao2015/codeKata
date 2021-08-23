fee4fd8d3b675f757988aec65decdeac1bab2191
wutaotao <531618500@qq.com>
2021-08-23 23:15:52 +0800

    reverse list

--- prolog/merge.pl
+++ prolog/merge.pl
@@ -1,3 +1,8 @@
 
 merge([], List, List).
 merge([Head|Tail1], List, [Head|Tail2]) :- merge(Tail1, List, Tail2).
+
+reverse([A], [A]).
+/** watch the results, use a series of true statements to express the algorithm**/
+reverse(Res, [Head|Tail]) :- reverse(Tmp, Tail), merge(Tmp, Head, Res).
+
