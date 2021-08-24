9f9b14fe4989b4ceadb87a4486c243427342c755
wutaotao <531618500@qq.com>
2021-08-24 17:30:41 +0800

    find the min element of list has not solved yet

--- prolog/merge.pl
+++ prolog/merge.pl
@@ -2,7 +2,17 @@
 merge([], List, List).
 merge([Head|Tail1], List, [Head|Tail2]) :- merge(Tail1, List, Tail2).
 
-reverse([A], [A]).
-/** watch the results, use a series of true statements to express the algorithm**/
-reverse(Res, [Head|Tail]) :- reverse(Tmp, Tail), merge(Tmp, Head, Res).
+rev([], []).
+/** watch each step of the recursive progress, use a series of true statements to 
+express the algorithm **/
+rev(Res, [Head|Tail]) :- reverse(Tmp, Tail), merge(Tmp, [Head], Res).
+
+min(Min, A, B) :- A < B, Min is A.
+min(Min, A, B) :- A = B, Min is A.
+min(Min, A, B) :- A > B, Min is B.
+
+minList(M, [A]) :- minList(A, [A]).
+% need to use tail recursion optimization here
+% minList(M, [Head|Tail]) :- minList(Tmp, Tail), min(M, Tmp, Head).
+% minList(M, [Head|Tail]) :- M < Head, minList(M, Tail).
 
