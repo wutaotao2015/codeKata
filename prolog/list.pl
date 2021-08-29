b216ec8892c9d07b5fdac8cec5efd2abae8eee3b
wutaotao <531618500@qq.com>
2021-08-29 20:44:03 +0800

    higher order program example with list map function

--- prolog/list.pl
+++ prolog/list.pl
@@ -7,3 +7,11 @@ sum(Total, [Head|Tail]) :- sum(TailTotal, Tail), Total is Head + TailTotal.
 
 avg(Result, List) :- count(Count, List), sum(Total, List), Result is Total / Count.
 
+
+% using call to implement higher order function
+map(F, [], []).
+map(F, [H|T], [RH|RT]) :-
+  call(F, H, RH),
+  map(F, T, RT).
+% map(plus(1), [1,2,3], R).
+
